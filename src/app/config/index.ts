77e72b10afd3def983c654f285a01c3d73ae9f20
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-11-30 21:25:41 +0600

    Some unnecessary files have been deleted.

--- src/app/config/index.ts
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-import dotenv from 'dotenv';
-
-dotenv.config({ path: path.join((process.cwd(), '.env')) });
-
-export default {
-  port: process.env.PORT,
-  databaseURI: process.env.DATABASE_URL,
-};
--- src/app/modules/order/order.interface.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-export interface IOrder extends Document {
-  email: string;
-  product: mongoose.Schema.Types.ObjectId;
-  quantity: number;
-  totalPrice: number;
-}
