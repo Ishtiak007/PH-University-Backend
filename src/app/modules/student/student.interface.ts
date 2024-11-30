6dda933730c337a1d171ec2bd497669b02136d93
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-01 00:28:30 +0600

    created interface for student

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,26 @@
+export type TGuardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type Student = {
+  id: string;
+  name: {
+    firstName: string;
+    middleName: string;
+    lastName: string;
+  };
+  gender: 'male' | 'female';
+  dateOfBirth: string;
+  contactNumber: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: TGuardian;
+};
