3f45984c89ffe1aa5943a21e231ccd699a820f7f
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-03 15:08:52 +0600

    Fix validation messages for required fields in schemas

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -9,60 +9,60 @@ import {
 const userNameSchema = new Schema<UserName>({
   firstName: {
     type: String,
-    required: true,
+    required: [true, 'This first name field is required'],
   },
   middleName: {
     type: String,
   },
   lastName: {
     type: String,
-    required: true,
+    required: [true, 'This last name field is required'],
   },
 });
 
 const guardianSchema = new Schema<TGuardian>({
   fatherName: {
     type: String,
-    required: true,
+    required: [true, 'This FatharName field is required'],
   },
   fatherOccupation: {
     type: String,
-    required: true,
+    required: [true, 'This fatherOccupation field is required'],
   },
   fatherContactNo: {
     type: String,
-    required: true,
+    required: [true, 'This fatherContactNo field is required'],
   },
   motherName: {
     type: String,
-    required: true,
+    required: [true, 'This motherName field is required'],
   },
   motherOccupation: {
     type: String,
-    required: true,
+    required: [true, 'This motherOccupation field is required'],
   },
   motherContactNo: {
     type: String,
-    required: true,
+    required: [true, 'This motherContactNo field is required'],
   },
 });
 
 const localGuardianSchema = new Schema<TLocalGuardian>({
   name: {
     type: String,
-    required: true,
+    required: [true, 'This localGuardianSchema field is required'],
   },
   occupation: {
     type: String,
-    required: true,
+    required: [true, 'This occupation field is required'],
   },
   contactNo: {
     type: String,
-    required: true,
+    required: [true, 'This contactNo field is required'],
   },
   address: {
     type: String,
-    required: true,
+    required: [true, 'This address field is required'],
   },
 });
 
@@ -71,25 +71,25 @@ const studentSchema = new Schema<Student>({
   id: String,
   name: {
     type: userNameSchema,
-    required: true,
+    required: [true, 'This name field is required'],
   },
   gender: {
     type: String,
     enum: ['male', 'female', 'other'],
-    required: true,
+    required: [true, 'This gender field is required'],
   },
   dateOfBirth: String,
   email: {
     type: String,
-    required: true,
+    required: [true, 'This dateOfBirth field is required'],
   },
   contactNo: {
     type: String,
-    required: true,
+    required: [true, 'This contactNo field is required'],
   },
   emergencyContactNo: {
     type: String,
-    required: true,
+    required: [true, 'This emergencyContactNo field is required'],
   },
   bloodGroup: {
     type: String,
@@ -97,8 +97,14 @@ const studentSchema = new Schema<Student>({
   },
   presentAddress: { type: String, required: true },
   permanentAddress: { type: String, required: true },
-  guardian: guardianSchema,
-  localGuardian: localGuardianSchema,
+  guardian: {
+    type: guardianSchema,
+    required: [true, 'This guardian field is required'],
+  },
+  localGuardian: {
+    type: localGuardianSchema,
+    required: [true, 'This localGuardian field is required'],
+  },
   profileImg: { type: String },
   isActive: {
     type: String,
