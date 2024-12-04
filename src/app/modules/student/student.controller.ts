ef1fb99abc22f1386760857de3a5a90294335cdb
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-05 00:28:06 +0600

    controller created for delete a student

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -75,8 +75,28 @@ const getSingleStudent = async (req: Request, res: Response) => {
   }
 };
 
+// delete a student from db
+const deleteStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+    const result = await studentServices.deleteStudentFromDb(studentId);
+    res.status(200).json({
+      success: true,
+      message: 'Student deleted successfully',
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message || 'something went worng',
+      data: err,
+    });
+  }
+};
+
 export const studentControllers = {
   createStudent,
   getAllStudents,
   getSingleStudent,
+  deleteStudent,
 };
