d50823979af82cda1ec90c118a329d902b6e6caf
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-04 10:56:57 +0600

    Organize and finalize Joi validation schema for student model

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -4,82 +4,17 @@ import Joi from 'joi';
 
 const createStudent = async (req: Request, res: Response) => {
   try {
-    // creating a validation schema using JOI
-    const userNameSchema = Joi.object({
-      firstName: Joi.string()
-        .trim()
-        .max(20)
-        .required()
-        .custom((value, helpers) => {
-          const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1);
-          if (firstNameStr !== value) {
-            return helpers.error('any.custom', {
-              message: `${value} is not in capitalize format`,
-            });
-          }
-          return value;
-        }, 'First name capitalization validation'),
-      middleName: Joi.string().trim().optional(),
-      lastName: Joi.string()
-        .required()
-        .custom((value, helpers) => {
-          if (!/^[a-zA-Z]+$/.test(value)) {
-            return helpers.error('any.custom', {
-              message: `${value} is not validddddddd`,
-            });
-          }
-          return value;
-        }, 'Last name alphabetic validation'),
-    });
-
-    // Guardian schema
-    const guardianSchema = Joi.object({
-      fatherName: Joi.string().required(),
-      fatherOccupation: Joi.string().required(),
-      fatherContactNo: Joi.string().required(),
-      motherName: Joi.string().required(),
-      motherOccupation: Joi.string().required(),
-      motherContactNo: Joi.string().required(),
-    });
-
-    // LocalGuardian schema
-    const localGuardianSchema = Joi.object({
-      name: Joi.string().required(),
-      occupation: Joi.string().required(),
-      contactNo: Joi.string().required(),
-      address: Joi.string().required(),
-    });
-
-    // Student schema
-    const studentSchema = Joi.object({
-      id: Joi.string().required(),
-      name: userNameSchema.required(),
-      gender: Joi.string()
-        .valid('male', 'female', 'other')
-        .required()
-        .messages({ 'any.only': '{#value} is not valid' }),
-      dateOfBirth: Joi.string().optional(),
-      email: Joi.string()
-        .email()
-        .required()
-        .messages({ 'string.email': '{#value} is not a valid email type' }),
-      contactNo: Joi.string().required(),
-      emergencyContactNo: Joi.string().required(),
-      bloodGroup: Joi.string()
-        .valid('A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-')
-        .optional(),
-      presentAddress: Joi.string().required(),
-      permanentAddress: Joi.string().required(),
-      guardian: guardianSchema.required(),
-      localGuardian: localGuardianSchema.required(),
-      profileImg: Joi.string().optional(),
-      isActive: Joi.string().valid('active', 'blocked').default('active'),
-    });
-
     const { student: studentData } = req.body;
 
     const { error, value } = studentSchema.validate(studentData);
-    console.log({ error }, { value });
+
+    if (error) {
+      res.status(500).json({
+        success: false,
+        message: 'Something went worng',
+        error: error.details,
+      });
+    }
 
     const result = await studentServices.createStudentIntoDB(studentData);
     res.status(200).json({
---
+++ src/app/modules/student/student.validation.ts
@@ -0,0 +1,75 @@
+import Joi from 'joi';
+
+// creating a validation schema using JOI
+const userNameSchema = Joi.object({
+  firstName: Joi.string()
+    .trim()
+    .max(20)
+    .required()
+    .custom((value, helpers) => {
+      const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1);
+      if (firstNameStr !== value) {
+        return helpers.error('any.custom', {
+          message: `${value} is not in capitalize format`,
+        });
+      }
+      return value;
+    }, 'First name capitalization validation'),
+  middleName: Joi.string().trim().optional(),
+  lastName: Joi.string()
+    .required()
+    .custom((value, helpers) => {
+      if (!/^[a-zA-Z]+$/.test(value)) {
+        return helpers.error('any.custom', {
+          message: `${value} is not validddddddd`,
+        });
+      }
+      return value;
+    }, 'Last name alphabetic validation'),
+});
+
+// Guardian schema
+const guardianSchema = Joi.object({
+  fatherName: Joi.string().required(),
+  fatherOccupation: Joi.string().required(),
+  fatherContactNo: Joi.string().required(),
+  motherName: Joi.string().required(),
+  motherOccupation: Joi.string().required(),
+  motherContactNo: Joi.string().required(),
+});
+
+// LocalGuardian schema
+const localGuardianSchema = Joi.object({
+  name: Joi.string().required(),
+  occupation: Joi.string().required(),
+  contactNo: Joi.string().required(),
+  address: Joi.string().required(),
+});
+
+// Student schema
+const studentvalidationSchema = Joi.object({
+  id: Joi.string().required(),
+  name: userNameSchema.required(),
+  gender: Joi.string()
+    .valid('male', 'female', 'other')
+    .required()
+    .messages({ 'any.only': '{#value} is not valid' }),
+  dateOfBirth: Joi.string().optional(),
+  email: Joi.string()
+    .email()
+    .required()
+    .messages({ 'string.email': '{#value} is not a valid email type' }),
+  contactNo: Joi.string().required(),
+  emergencyContactNo: Joi.string().required(),
+  bloodGroup: Joi.string()
+    .valid('A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-')
+    .optional(),
+  presentAddress: Joi.string().required(),
+  permanentAddress: Joi.string().required(),
+  guardian: guardianSchema.required(),
+  localGuardian: localGuardianSchema.required(),
+  profileImg: Joi.string().optional(),
+  isActive: Joi.string().valid('active', 'blocked').default('active'),
+});
+
+export default studentvalidationSchema;
