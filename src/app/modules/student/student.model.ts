9f5a210aa3129a0f4088c843cd7cb2ba43e3ba66
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-05 10:25:02 +0600

    Add pre-findOne middleware to exclude soft-deleted students from query results

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -189,6 +189,12 @@ studentSchema.post('save', function (doc, next) {
 // Query middleware
 studentSchema.pre('find', function (next) {
   // console.log(this);
+  this.find({ isDeleted: { $ne: true } });
+  next();
+});
+studentSchema.pre('findOne', function (next) {
+  // console.log(this);
+  this.find({ isDeleted: { $ne: true } });
   next();
 });
 
