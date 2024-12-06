8aca43841834ce851c1874b789bbed280baa0955
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-07 00:07:22 +0600

    implement User schema with validation and timestamps

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,29 +1,36 @@
-import { Schema } from 'mongoose';
+import { model, Schema } from 'mongoose';
 import { TUser } from './user.interface';
 
-const userSchema = new Schema<TUser>({
-  id: {
-    type: String,
-    required: true,
+const userSchema = new Schema<TUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      default: true,
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'student', 'faculty'],
+    },
+    status: {
+      type: String,
+      enum: ['in-progress', 'blocked'],
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  password: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
   },
-  needsPasswordChange: {
-    type: Boolean,
-    default: true,
-  },
-  role: {
-    type: String,
-    enum: ['admin', 'student', 'faculty'],
-  },
-  status: {
-    type: String,
-    enum: ['in-progress', 'blocked'],
-  },
-  isDeleted: {
-    type: Boolean,
-    default: false,
-  },
-});
+);
+
+export const User = model<TUser>('User', userSchema);
