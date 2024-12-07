252e9452bd798379c1a7a11102afe49df1dacf87
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-07 20:33:23 +0600

    use sendResponse utility for consistent API response format

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { UserServices } from './user.service';
+import sendResponse from '../../utils/sendResponse';
+import httpStatus from 'http-status';
 
 const createStudent = async (
   req: Request,
@@ -17,9 +19,15 @@ const createStudent = async (
       studentData,
     );
 
-    res.status(200).json({
+    // res.status(200).json({
+    //   success: true,
+    //   message: 'Student created successfully',
+    //   data: result,
+    // });
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
       success: true,
-      message: 'Student created successfully',
+      message: 'Student Created Successfully',
       data: result,
     });
   } catch (err) {
