50902ba81f89097ffacb139647eecba122756c8a
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-07 11:53:04 +0600

    route setup in app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 import cors from 'cors';
 import { StudentRoutes } from './app/modules/student/student.route';
+import { UserRoutes } from './app/modules/user/user.route';
 
 // Parsers
 app.use(express.json());
 app.use(cors());
 
 app.use('/api/v1/students', StudentRoutes);
+
+app.use('/api/v1/users', UserRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send(
