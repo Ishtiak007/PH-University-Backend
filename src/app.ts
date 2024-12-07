a14225ab3aa6f87de5a868033cf1afaaddd73d73
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-07 19:54:40 +0600

    add global error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
-import express, { Application, Request, Response } from 'express';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import express, { Application, Request, Response, NextFunction } from 'express';
 const app: Application = express();
 import cors from 'cors';
 import { StudentRoutes } from './app/modules/student/student.route';
@@ -16,6 +18,16 @@ app.get('/', (req: Request, res: Response) => {
   res.send(
     'Hello developers, I am Ishtiak From Rangpur, I am developing PH-University 🦄!',
   );
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  const statusCode = 500;
+  const message = err.message || 'Something went worng';
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    error: err,
+  });
 });
 
 export default app;
