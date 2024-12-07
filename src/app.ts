9fb07819e00c19d05a4c03284ea4d590f34ec043
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-07 20:00:59 +0600

    globalErrorhandler file organized

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 import cors from 'cors';
 import { StudentRoutes } from './app/modules/student/student.route';
 import { UserRoutes } from './app/modules/user/user.route';
+import globalErrorHandler from './app/middlewares/globalErrorhandler';
 
 // Parsers
 app.use(express.json());
@@ -20,14 +19,6 @@ app.get('/', (req: Request, res: Response) => {
   );
 });
 
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  const statusCode = 500;
-  const message = err.message || 'Something went worng';
-  return res.status(statusCode).json({
-    success: false,
-    message,
-    error: err,
-  });
-});
+app.use(globalErrorHandler);
 
 export default app;
