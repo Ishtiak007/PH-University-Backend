2807dcc0e7fba456d43b06df4f6b1b8b0fbca35c
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-08 13:27:43 +0600

    isActive: z.enum(['active', 'blocked']).default('active'),     isDeleted: z.boolean(), deleted

--- src/app/modules/student/student.validation.ts
+++ src/app/modules/student/student.validation.ts
@@ -67,8 +67,6 @@ export const studentValidationSchema = z.object({
     guardian: guardianValidationSchema,
     localGuardian: localGuardianValidationSchema,
     profileImg: z.string().optional(),
-    isActive: z.enum(['active', 'blocked']).default('active'),
-    isDeleted: z.boolean(),
   }),
 });
 
