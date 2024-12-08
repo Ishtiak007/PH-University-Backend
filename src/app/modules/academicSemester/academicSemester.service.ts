222559ab123a8c1c54c407e1bb1d93aa8e7ae87d
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-08 21:36:48 +0600

    add validation for academic semester name and code mapping

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -2,6 +2,20 @@ import { TAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
 
 const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
+  // semester name ------> semester code
+  type TAcademicSemesterNameCodeMapper = {
+    [key: string]: string;
+  };
+  const academicSemesterNameCodeMapper: TAcademicSemesterNameCodeMapper = {
+    Autumn: '01',
+    Summar: '02',
+    Fall: '03',
+  };
+
+  if (academicSemesterNameCodeMapper[payload.name] !== payload.code) {
+    throw new Error('Invalid semester code');
+  }
+
   const result = await AcademicSemester.create(payload);
   return result;
 };
