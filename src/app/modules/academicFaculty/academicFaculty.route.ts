408de177881b5213a21b6cbb93082c317df99ffa
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-16 09:25:20 +0600

    add routes for academic faculty creation and retrieval

---
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { academicFacultyValidation } from './academicFaculty.Validation';
+import { AcademicFacultyControllers } from './academicFaculty.controller';
+const router = express.Router();
+
+router.post(
+  '/create-academic-faculty',
+  validateRequest(
+    academicFacultyValidation.createAcademicFacultyValidationSchema,
+  ),
+  AcademicFacultyControllers.createAcademicFaculty,
+);
+
+router.get('/', AcademicFacultyControllers.getAllAcademicFaculties);
+
+export const AcademicSemesterRoutes = router;
