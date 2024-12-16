e925340a278a15cbaa7e81dc9340fd33fb7a73af
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-16 15:32:48 +0600

    implement custom AppError class for error handling

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -29,13 +29,26 @@ academicDepartmentSchema.pre('save', async function (next) {
   next();
 });
 
+class AppError extends Error {
+  public statusCode: number;
+  constructor(statusCode: number, message: string, stack = '') {
+    super(message);
+    this.statusCode = statusCode;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
 academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
   const query = this.getQuery();
 
   const isDepartmentExist = await AcademicDepartment.findOne(query);
 
   if (!isDepartmentExist) {
-    throw new Error('This department does not exist bro');
+    throw new AppError(404, 'This department does not exist bro');
   }
   next();
 });
