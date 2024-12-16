9c5c80ae600a1bede28fa9168060ebd2769f82dd
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-16 11:50:31 +0600

    add routes for academic department management

---
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { academicDepartmentValidation } from './academicDepartment.validation';
+import { AcademicDepartmentControllers } from './academicDepartment.controller';
+const router = express.Router();
+
+router.post(
+  '/create-academic-department',
+  validateRequest(
+    academicDepartmentValidation.createAcademicDepartmentValidationSchema,
+  ),
+  AcademicDepartmentControllers.createAcademicDepartment,
+);
+
+router.get(
+  '/:departmentId',
+  AcademicDepartmentControllers.getSingleAcademicDepartment,
+);
+
+router.patch(
+  '/:departmentId',
+  validateRequest(
+    academicDepartmentValidation.updateAcatdemicDepartmentValidationSchema,
+  ),
+  AcademicDepartmentControllers.updateAcademicDepartment,
+);
+
+router.get('/', AcademicDepartmentControllers.getAllAcademicDepartments);
+
+export const AcademicDepartmentRoutes = router;
