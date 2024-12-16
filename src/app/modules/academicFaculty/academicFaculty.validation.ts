66e834f221b02bc42da56ab5378a1d6dc64da112
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-16 09:43:19 +0600

    updated academic faculty validations

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,12 +1,14 @@
 import { z } from 'zod';
 
 const createAcademicFacultyValidationSchema = z.object({
-  name: z.string({ invalid_type_error: 'Academic faculty must be a string' }),
+  body: z.object({
+    name: z.string({ invalid_type_error: 'Academic faculty must be a string' }),
+  }),
 });
 
 const updateAcatdemicFucaltyValidationSchema = z.object({
-  name: z.string({
-    invalid_type_error: 'Academic faculty must be string',
+  body: z.object({
+    name: z.string({ invalid_type_error: 'Academic faculty must be a string' }),
   }),
 });
 
