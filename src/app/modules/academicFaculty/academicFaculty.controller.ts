eb82b50ee58fdc05645804f6ab6e8271d048fda1
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-16 09:09:00 +0600

    add controllers for academic faculty (create, retrieve, update)

---
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -0,0 +1,62 @@
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import httpStatus from 'http-status';
+import { AcademicSemesterServices } from './academicFaculty.service';
+
+const createAcademicFaculty = catchAsync(async (req, res) => {
+  const result = await AcademicSemesterServices.createAcademicFacultyIntoDB(
+    req.body,
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculty is Created Successfully',
+    data: result,
+  });
+});
+
+const getAllAcademicFaculties = catchAsync(async (req, res) => {
+  const result = await AcademicSemesterServices.getAllAcademicFacultiesFromDB();
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculty are retrieved successfully',
+    data: result,
+  });
+});
+
+const getSingleAcademicFaculty = catchAsync(async (req, res) => {
+  const { semesterId } = req.params;
+  const result =
+    await AcademicSemesterServices.getSingleAcademicFacultyFromDB(semesterId);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculty is retrieved succesfully',
+    data: result,
+  });
+});
+
+const updateAcademicFaculty = catchAsync(async (req, res) => {
+  const { semesterId } = req.params;
+  const result = await AcademicSemesterServices.updateAcademicFacultyIntoDB(
+    semesterId,
+    req.body,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculty is retrieved succesfully',
+    data: result,
+  });
+});
+
+export const AcademicSemesterControllers = {
+  createAcademicFaculty,
+  getAllAcademicFaculties,
+  getSingleAcademicFaculty,
+  updateAcademicFaculty,
+};
