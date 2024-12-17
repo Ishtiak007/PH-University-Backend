3904b256505fd976af1c24f66f90e70b7440da2a
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-17 21:49:24 +0600

    Add generic Error handling for unexpected errors

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -13,8 +13,8 @@ import AppError from '../errors/AppError';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   // setting default values
-  let statusCode = err.statusCode || 500;
-  let message = err.message || 'Something went worng';
+  let statusCode = 500;
+  let message = 'Something went worng';
 
   let errorSources: TErrorSources = [
     {
@@ -45,6 +45,14 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     errorSources = simplifiedError?.errorSources;
   } else if (err instanceof AppError) {
     statusCode = err?.statusCode;
+    message = err?.message;
+    errorSources = [
+      {
+        path: '',
+        message: err?.message,
+      },
+    ];
+  } else if (err instanceof Error) {
     message = err?.message;
     errorSources = [
       {
