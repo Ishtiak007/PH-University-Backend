1263951bf316fdccf26cec4fb56bc09b476af3b8
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-18 21:09:29 +0600

    user.service and user.utils updated

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -15,17 +15,17 @@ const findLastStudentId = async () => {
       createdAt: -1,
     })
     .lean();
-  // 203001     0001
+
   return lastStudent?.id ? lastStudent.id : undefined;
 };
 
-// year, semesterCode, 4 digit number;
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  //first time 000
   let currentId = (0).toString();
   const lastStudentId = await findLastStudentId();
+
   const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
   const lastStudentYear = lastStudentId?.substring(0, 4);
+
   const currentSemesterCode = payload.code;
   const currentYear = payload.year;
 
@@ -38,6 +38,75 @@ export const generateStudentId = async (payload: TAcademicSemester) => {
   }
 
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+
   incrementId = `${payload.year}${payload.code}${incrementId}`;
+
+  return incrementId;
+};
+
+// Faculty ID
+export const findLastFacultyId = async () => {
+  const lastFaculty = await User.findOne(
+    {
+      role: 'faculty',
+    },
+    {
+      id: 1,
+      _id: 0,
+    },
+  )
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
+
+  return lastFaculty?.id ? lastFaculty.id.substring(2) : undefined;
+};
+
+export const generateFacultyId = async () => {
+  let currentId = (0).toString();
+  const lastFacultyId = await findLastFacultyId();
+
+  if (lastFacultyId) {
+    currentId = lastFacultyId.substring(2);
+  }
+
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+
+  incrementId = `F-${incrementId}`;
+
+  return incrementId;
+};
+
+// Admin ID
+export const findLastAdminId = async () => {
+  const lastAdmin = await User.findOne(
+    {
+      role: 'admin',
+    },
+    {
+      id: 1,
+      _id: 0,
+    },
+  )
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
+
+  return lastAdmin?.id ? lastAdmin.id.substring(2) : undefined;
+};
+
+export const generateAdminId = async () => {
+  let currentId = (0).toString();
+  const lastAdminId = await findLastAdminId();
+
+  if (lastAdminId) {
+    currentId = lastAdminId.substring(2);
+  }
+
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+
+  incrementId = `A-${incrementId}`;
   return incrementId;
 };
