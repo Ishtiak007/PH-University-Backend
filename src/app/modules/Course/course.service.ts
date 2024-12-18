2657ba608227ba4376074fd1919b00f3637b7ee4
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-18 23:50:21 +0600

    Add course CRUD operations with soft delete functionality

---
+++ src/app/modules/Course/course.service.ts
@@ -0,0 +1,34 @@
+import { Course } from './course.model';
+
+const createCourseIntoDB = async () => {
+  const result = await Course.create();
+  return result;
+};
+
+const getAllCoursesFromDB = async () => {
+  const result = await Course.find();
+  return result;
+};
+
+const getSingleCoursesFromDB = async (id: string) => {
+  const result = await Course.findById(id);
+  return result;
+};
+
+const deleteCoursesIntoDB = async (id: string) => {
+  const result = await Course.findByIdAndUpdate(
+    id,
+    { isDeleted: true },
+    {
+      new: true,
+    },
+  );
+  return result;
+};
+
+export const CourseServices = {
+  createCourseIntoDB,
+  getAllCoursesFromDB,
+  getSingleCoursesFromDB,
+  deleteCoursesIntoDB,
+};
