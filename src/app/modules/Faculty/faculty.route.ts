31da0310f6985888767bf522f8eff9186d33b5cd
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-18 19:40:27 +0600

    Implement Faculty schema with Mongoose, virtuals, pre-hooks, and static methods

--- src/app/modules/Faculty/faculty.route.ts
+++ src/app/modules/Faculty/faculty.route.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
+import { FacultyControllers } from './faculty.controller';
 
 const router = express.Router();
 
