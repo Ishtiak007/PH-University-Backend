eaedaa7617a5f90de1c049f1d754915d35f1587e
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-19 00:22:41 +0600

    Add Course Controller with CRUD operations

---
+++ src/app/modules/Course/course.controller.ts
@@ -0,0 +1,71 @@
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import httpStatus from 'http-status';
+import { CourseServices } from './course.service';
+
+const createCourse = catchAsync(async (req, res) => {
+  const result = await CourseServices.createCourseIntoDB(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Course is Created Successfully',
+    data: result,
+  });
+});
+
+const getAllCourses = catchAsync(async (req, res) => {
+  const result = await CourseServices.getAllCoursesFromDB();
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Courses are retrieved successfully',
+    data: result,
+  });
+});
+
+const getSingleCourse = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await CourseServices.getSingleCoursesFromDB(id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Course is retrieved succesfully done',
+    data: result,
+  });
+});
+
+// const updateCourse = catchAsync(async (req, res) => {
+//   const { id } = req.params;
+//   const result = await CourseServices.up(
+//     id,
+//     req.body,
+//   );
+
+//   sendResponse(res, {
+//     statusCode: httpStatus.OK,
+//     success: true,
+//     message: 'Course is updated succesfully',
+//     data: result,
+//   });
+// });
+
+const deleteCourse = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await CourseServices.deleteCoursesIntoDB(id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Course is deleted succesfully done',
+    data: result,
+  });
+});
+
+export const AcademicFacultyControllers = {
+  createCourse,
+  getAllCourses,
+  getSingleCourse,
+  deleteCourse,
+};
