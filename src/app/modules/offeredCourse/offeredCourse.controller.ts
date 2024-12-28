a075d253e8ef139a977b0aa40d7def27f86ba89c
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-28 23:03:08 +0600

    Add createOfferedCourse controller to handle offered course creation requests

--- src/app/modules/offeredCourse/offeredCourse.controller.ts
+++ src/app/modules/offeredCourse/offeredCourse.controller.ts
@@ -2,12 +2,20 @@ import { Request, Response } from 'express';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import httpStatus from 'http-status';
+import { OfferedCourseServices } from './offeredCourse.service';
 
 const createOfferedCourse = catchAsync(async (req: Request, res: Response) => {
-  const result = await sendResponse(res, {
+  const result = await OfferedCourseServices.createOfferedCourseIntoDB(
+    req.body,
+  );
+  sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'Offered course is created successfully!',
     data: result,
   });
 });
+
+export const OfferedCourseControllers = {
+  createOfferedCourse,
+};
