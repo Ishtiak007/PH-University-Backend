4c8f6fc118e2bc26f55682ede6084130d7ee3dec
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-28 11:25:58 +0600

    create semesterRegistration.constant file

--- src/app/modules/semesterRegistration/semesterRegistration.model.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.model.ts
@@ -1,11 +1,23 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 import { TSemesterRegisteration } from './semesterRegistration.interface';
 
-const semesterRegistrationSchema = new mongoose.Schema<TSemesterRegisteration>(
-  {},
-);
+const semesterRegistrationSchema = new mongoose.Schema<TSemesterRegisteration>({
+  academicSemester: {
+    type: Schema.Types.ObjectId,
+    ref: 'AcademicSemester',
+    unique: true,
+    required: true,
+  },
+});
 
 export const SemesterRegistration = mongoose.model<TSemesterRegisteration>(
   'SemesterRegistration',
   semesterRegistrationSchema,
 );
+
+academicSemester: Types.ObjectId;
+status: 'UPCOMING' | 'ONGOING' | 'ENDED';
+startDate: Date;
+endDate: Date;
+minCredit: number;
+maxCredit: number;
