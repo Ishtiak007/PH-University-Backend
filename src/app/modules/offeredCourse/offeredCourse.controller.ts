423d843d24a8d227b38e5cf39f49e3853a2186dc
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-28 22:45:18 +0600

    Basic boiler plate for the offeredCourse

---
+++ src/app/modules/offeredCourse/offeredCourse.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import httpStatus from 'http-status';
+
+const createOfferedCourse = catchAsync(async (req: Request, res: Response) => {
+  const result = await sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Offered course is created successfully!',
+    data: result,
+  });
+});
