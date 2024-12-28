e57552e3fd62852b7fcc741edcb6eb2847d99617
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-28 11:29:08 +0600

    Create SemesterRegistration Mongoose schema and model

--- src/app/modules/semesterRegistration/semesterRegistration.model.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.model.ts
@@ -1,23 +1,43 @@
 import mongoose, { Schema } from 'mongoose';
 import { TSemesterRegisteration } from './semesterRegistration.interface';
+import { SemesterRegistrationStatus } from './semesterRegistration.constant';
 
-const semesterRegistrationSchema = new mongoose.Schema<TSemesterRegisteration>({
-  academicSemester: {
-    type: Schema.Types.ObjectId,
-    ref: 'AcademicSemester',
-    unique: true,
-    required: true,
+const semesterRegistrationSchema = new mongoose.Schema<TSemesterRegisteration>(
+  {
+    academicSemester: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicSemester',
+      unique: true,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: SemesterRegistrationStatus,
+      default: 'UPCOMING',
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    minCredit: {
+      type: Number,
+      default: 3,
+    },
+    maxCredit: {
+      type: Number,
+      default: 15,
+    },
   },
-});
+  {
+    timestamps: true,
+  },
+);
 
 export const SemesterRegistration = mongoose.model<TSemesterRegisteration>(
   'SemesterRegistration',
   semesterRegistrationSchema,
 );
-
-academicSemester: Types.ObjectId;
-status: 'UPCOMING' | 'ONGOING' | 'ENDED';
-startDate: Date;
-endDate: Date;
-minCredit: number;
-maxCredit: number;
