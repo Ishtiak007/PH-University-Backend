2336317a76a20f4698390202ee9611eefa22520d
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-12-29 21:39:40 +0600

    Add utility functions to generate unique IDs for students, faculty, and admins

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -79,34 +79,34 @@ export const generateFacultyId = async () => {
 };
 
 // Admin ID
-// export const findLastAdminId = async () => {
-//   const lastAdmin = await User.findOne(
-//     {
-//       role: 'admin',
-//     },
-//     {
-//       id: 1,
-//       _id: 0,
-//     },
-//   )
-//     .sort({
-//       createdAt: -1,
-//     })
-//     .lean();
+export const findLastAdminId = async () => {
+  const lastAdmin = await User.findOne(
+    {
+      role: 'admin',
+    },
+    {
+      id: 1,
+      _id: 0,
+    },
+  )
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
 
-//   return lastAdmin?.id ? lastAdmin.id.substring(2) : undefined;
-// };
+  return lastAdmin?.id ? lastAdmin.id.substring(2) : undefined;
+};
 
-// export const generateAdminId = async () => {
-//   let currentId = (0).toString();
-//   const lastAdminId = await findLastAdminId();
+export const generateAdminId = async () => {
+  let currentId = (0).toString();
+  const lastAdminId = await findLastAdminId();
 
-//   if (lastAdminId) {
-//     currentId = lastAdminId.substring(2);
-//   }
+  if (lastAdminId) {
+    currentId = lastAdminId.substring(2);
+  }
 
-//   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
-//   incrementId = `A-${incrementId}`;
-//   return incrementId;
-// };
+  incrementId = `A-${incrementId}`;
+  return incrementId;
+};
