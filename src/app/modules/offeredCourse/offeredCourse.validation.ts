0ab2f63a7199a9e7db12d6cbde2be2e94facf22a
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-01-03 12:04:53 +0600

    Add Zod schema for validating 24-hour time format (HH:MM).

--- src/app/modules/offeredCourse/offeredCourse.validation.ts
+++ src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -1,6 +1,16 @@
 import { z } from 'zod';
 import { Days } from './offeredCourse.constant';
 
+const timeStringSchema = z.string().refine(
+  (time) => {
+    const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/; // 00-09 10-19 20-23
+    return regex.test(time);
+  },
+  {
+    message: 'Invalid time format , expected "HH:MM" in 24 hours format',
+  },
+);
+
 const createOfferedCourseValidationSchema = z.object({
   body: z.object({
     semesterRegistration: z.string(),
@@ -11,7 +21,7 @@ const createOfferedCourseValidationSchema = z.object({
     section: z.number(),
     maxCapacity: z.number(),
     days: z.array(z.enum([...Days] as [string, ...string[]])),
-    startTime: z.string(), // HH: MM   00-23: 00-59
+    startTime: timeStringSchema, // HH: MM   00-23: 00-59
     endTime: z.string(),
   }),
 });
