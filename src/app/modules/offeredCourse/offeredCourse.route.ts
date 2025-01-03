b93d0659bb49d688c1b9b3d09e8ceb10a81c6874
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-01-03 22:35:45 +0600

    Add routes for updating and deleting offered courses by ID

--- src/app/modules/offeredCourse/offeredCourse.route.ts
+++ src/app/modules/offeredCourse/offeredCourse.route.ts
@@ -5,7 +5,7 @@ import { OfferedCourseValidations } from './offeredCourse.validation';
 
 const router = express.Router();
 
-// router.get('/', OfferedCourseControllers.getAllOfferedCourses);
+router.get('/', OfferedCourseControllers.getAllOfferedCourses);
 
 // router.get('/:id', OfferedCourseControllers.getSingleOfferedCourses);
 
@@ -15,12 +15,12 @@ router.post(
   OfferedCourseControllers.createOfferedCourse,
 );
 
-// router.patch(
-//   '/:id',
-//   validateRequest(OfferedCourseValidations.updateOfferedCourseValidationSchema),
-//   OfferedCourseControllers.updateOfferedCourse,
-// );
+router.patch(
+  '/:id',
+  validateRequest(OfferedCourseValidations.updateOfferedCourseValidationSchema),
+  OfferedCourseControllers.updateOfferedCourse,
+);
 
-// router.delete('/:id', OfferedCourseControllers.deleteOfferedCourseFromDB);
+router.delete('/:id', OfferedCourseControllers.deleteOfferedCourseFromDB);
 
 export const offeredCourseRoutes = router;
