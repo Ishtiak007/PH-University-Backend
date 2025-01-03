29c82753a8c125e402d5036c38ec7cf506b6b7ac
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-01-03 21:26:38 +0600

    Add time conflict validation for faculty schedules in offered courses

--- src/app/modules/offeredCourse/offeredCourse.service.ts
+++ src/app/modules/offeredCourse/offeredCourse.service.ts
@@ -108,8 +108,15 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
   assignedSchedules.forEach((schedule) => {
     const existingStartTime = new Date(`1970-01-01T${schedule.startTime}`);
     const existingEndTime = new Date(`1970-01-01T${schedule.endTime}`);
-    const newStartingTime = new Date(`1970-01-01T${newSchedule.startTime}`);
-    const newEndingTime = new Date(`1970-01-01T${newSchedule.endTime}`);
+    const newStartTime = new Date(`1970-01-01T${newSchedule.startTime}`);
+    const newEndTime = new Date(`1970-01-01T${newSchedule.endTime}`);
+
+    if (newStartTime < existingEndTime && newEndTime > existingStartTime) {
+      throw new AppError(
+        httpStatus.CONFLICT,
+        `This faculty is not available at that time ! Choose other time or date!`,
+      );
+    }
   });
 
   const result = await OfferedCourse.create({ ...payload, academicSemester });
