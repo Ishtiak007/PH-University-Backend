d7366dc3da3da4c26c5c3445479eecf0a067aaa4
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-01-04 23:25:03 +0600

    Add loginUser function in AuthServices for user authentication with error handling

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,7 +1,23 @@
+import AppError from '../../errors/AppError';
+import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
+import httpStatus from 'http-status';
 
 const loginUser = async (payload: TLoginUser) => {
-  console.log(payload);
+  //checking if the user is exists
+  const isUserExists = await User.findOne({ id: payload?.id });
+
+  if (!isUserExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'This user is not found');
+  }
+
+  // checking if the use is already deleted
+
+  const isDeleted = isUserExists?.isDeleted;
+
+  if (isUserExists) {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted');
+  }
   return {};
 };
 
