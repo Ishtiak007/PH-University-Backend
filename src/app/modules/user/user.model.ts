e1c7396bf9ff7c98a5fe89229ca3d4bf9526c910
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-01-04 23:49:27 +0600

    import UserModel

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,8 +1,8 @@
 import bcrypt from 'bcrypt';
 import { Schema, model } from 'mongoose';
 import config from '../../config';
-import { TUser } from './user.interface';
-const userSchema = new Schema<TUser>(
+import { TUser, UserModel } from './user.interface';
+const userSchema = new Schema<TUser, UserModel>(
   {
     id: {
       type: String,
@@ -53,4 +53,4 @@ userSchema.post('save', function (doc, next) {
   next();
 });
 
-export const User = model<TUser>('User', userSchema);
+export const User = model<TUser, UserModel>('User', userSchema);
