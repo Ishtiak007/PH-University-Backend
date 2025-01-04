97a690a39e9a106e71bd55808244ace497e01fa9
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-01-04 10:57:38 +0600

    Add controller to handle deletion of offered courses with response handling

--- src/app/modules/offeredCourse/offeredCourse.controller.ts
+++ src/app/modules/offeredCourse/offeredCourse.controller.ts
@@ -54,22 +54,23 @@ const updateOfferedCourse = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
-// const deleteOfferedCourseFromDB = catchAsync(
-//   async (req: Request, res: Response) => {
-//     const { id } = req.params;
-//     const result = await OfferedCourseServices.(id);
-//     sendResponse(res, {
-//       statusCode: httpStatus.OK,
-//       success: true,
-//       message: 'OfferedCourse deleted successfully',
-//       data: result,
-//     });
-//   },
-// );
+const deleteOfferedCourseFromDB = catchAsync(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await OfferedCourseServices.deleteOfferedCourseFromDB(id);
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'OfferedCourse deleted successfully',
+      data: result,
+    });
+  },
+);
 
 export const OfferedCourseControllers = {
   createOfferedCourse,
   getAllOfferedCourses,
   getSingleOfferedCourses,
   updateOfferedCourse,
+  deleteOfferedCourseFromDB,
 };
