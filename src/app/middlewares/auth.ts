4dd7b50be8fdfc10162bf65545f3b90fd90b929b
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-01-05 11:24:54 +0600

    Add JWT token verification to auth middleware for authorization validation

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -2,13 +2,33 @@ import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../utils/catchAsync';
 import AppError from '../errors/AppError';
 import httpStatus from 'http-status';
+import jwt from 'jsonwebtoken';
+import config from '../config';
 
 const auth = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
+
+    // if the token is sent from client
     if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
     }
+
+    // check if the token is valid
+    jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+      function (err, decoded) {
+        if (err) {
+          throw new AppError(
+            httpStatus.UNAUTHORIZED,
+            'Your are not authorized',
+          );
+        }
+        console.log(decoded);
+      },
+    );
+
     next();
   });
 };
