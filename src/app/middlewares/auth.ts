3b0f02b2ea95321165ac763597b35dfe237cea15
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-01-05 11:10:35 +0600

    Create auth middleware to log authorization headers

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -4,6 +4,7 @@ import catchAsync from '../utils/catchAsync';
 const auth = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     console.log(req.headers.authorization);
+    next();
   });
 };
 
