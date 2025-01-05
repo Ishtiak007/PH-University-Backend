1556935240f068531025cf111925aecb12908093
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-01-05 11:14:14 +0600

    Enhance auth middleware to handle missing authorization token with error response

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,9 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../utils/catchAsync';
+import AppError from '../errors/AppError';
+import httpStatus from 'http-status';
 
 const auth = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.headers.authorization);
+    const token = req.headers.authorization;
+    if (!token) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
+    }
     next();
   });
 };
