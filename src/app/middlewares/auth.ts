81eb20774d80936cf7b3454e2be39b5b7f6ac258
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-01-06 11:15:06 +0600

    Extend UserModel interface with methods for user existence, password matching, and JWT validation

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -5,6 +5,7 @@ import httpStatus from 'http-status';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
 import { TUserRole } from '../modules/user/user.interface';
+import { User } from '../modules/user/user.model';
 
 const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
@@ -16,9 +17,32 @@ const auth = (...requiredRoles: TUserRole[]) => {
     }
 
     // check if the token is valid
-    const decoded = jwt.verify(token, config.jwt_access_secret as string);
+    const decoded = jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+    ) as JwtPayload;
 
-    const role = (decoded as JwtPayload).role;
+    const { role, userId, iat } = decoded;
+
+    const user = await User.isUserExistsByCustomId(userId);
+
+    if (!user) {
+      throw new AppError(httpStatus.NOT_FOUND, 'This user is not found');
+    }
+
+    // checking if the use is already deleted
+    const isDeleted = user?.isDeleted;
+
+    if (isDeleted) {
+      throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted');
+    }
+
+    // checking if the use is blocked
+    const userStatus = user?.status;
+
+    if (userStatus === 'blocked') {
+      throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked');
+    }
 
     if (requiredRoles && !requiredRoles.includes(role)) {
       throw new AppError(
