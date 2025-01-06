b5b14e5427cd3be44dc53586a5257bfcf8b28159
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-01-06 09:14:31 +0600

    Implement changePassword function with validation, password matching, and update functionality

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -2,8 +2,9 @@ import AppError from '../../errors/AppError';
 import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
 import httpStatus from 'http-status';
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../../config';
+import bcrypt from 'bcrypt';
 
 const loginUser = async (payload: TLoginUser) => {
   //checking if the user is exists
@@ -47,7 +48,55 @@ const loginUser = async (payload: TLoginUser) => {
   };
 };
 
-const changePassword = () => {};
+const changePassword = async (
+  userData: JwtPayload,
+  payload: {
+    oldPassword: string;
+    newPassword: string;
+  },
+) => {
+  const user = await User.isUserExistsByCustomId(userData.id);
+
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, 'This user is not found');
+  }
+
+  // checking if the use is already deleted
+  const isDeleted = user?.isDeleted;
+
+  if (isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted');
+  }
+
+  // checking if the use is blocked
+  const userStatus = user?.status;
+
+  if (userStatus === 'blocked') {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked');
+  }
+
+  // checking if the password is correct
+  if (!(await User.isPasswordMatched(payload.oldPassword, user?.password)))
+    //Access granted : send accesstoken, refressToken
+    throw new AppError(httpStatus.FORBIDDEN, 'Password do not matched!');
+
+  //hash new password
+  const newHashedPassword = await bcrypt.hash(
+    payload.newPassword,
+    Number(config.bcrypt_salt_rounds),
+  );
+
+  await User.findOneAndUpdate(
+    {
+      id: userData.userId,
+      role: userData.role,
+    },
+    {
+      password: newHashedPassword,
+    },
+  );
+  return null;
+};
 
 export const AuthServices = {
   loginUser,
