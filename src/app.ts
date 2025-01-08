5d708da1477ada8a44d6287a639b0796e9fdadb1
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-01-08 23:29:22 +0600

    Set up Express application with middleware, routes, and error handling

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,34 @@
-import express, { Application, Request, Response } from 'express';
-const app: Application = express();
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import express, { Application } from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorhandler';
 import notFound from './app/middlewares/notFound';
 import router from './app/routes';
 
-// Parsers
-app.use(express.json());
-app.use(cors());
+const app: Application = express();
 
+//parsers
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: ['http://localhost:5173'] }));
+
+// application routes
 app.use('/api/v1', router);
 
-const test = async (req: Request, res: Response) => {
-  Promise.reject();
-  // const a = 10;
-  // res.send(a);
-};
+// const test = async (req: Request, res: Response) => {
+//   const a = 10;
+//   res.send(a);
+// };
 
-app.get('/', test);
+// app.get('/', test);
 
 app.use(globalErrorHandler);
 
-// not found middleware
+//Not Found
 app.use(notFound);
 
 export default app;
