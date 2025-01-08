4ab32dfb65fd792abc15b5c9e4c517d52a0835ae
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-01-08 23:15:53 +0600

    Send response with accessToken and needsPasswordChange flag on successful login

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -6,7 +6,7 @@ import config from '../../config';
 
 const loginUser = catchAsync(async (req, res) => {
   const result = await AuthServices.loginUser(req.body);
-  const { refreshToken } = result;
+  const { refreshToken, accessToken, needsPasswordChange } = result;
 
   res.cookie('refreshToken', refreshToken, {
     secure: config.NODE_ENV === 'production',
@@ -16,8 +16,11 @@ const loginUser = catchAsync(async (req, res) => {
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'User is logged in successfully!',
-    data: result,
+    message: 'User is logged in successfully',
+    data: {
+      accessToken,
+      needsPasswordChange,
+    },
   });
 });
 
