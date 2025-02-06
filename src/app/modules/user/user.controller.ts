4286d7c2963f11ccdd2b05e56f2fc023837ec664
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-02-06 21:58:13 +0600

    Enhanced getMe function to populate user details for student, admin, and faculty

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -44,13 +44,15 @@ const createAdmin = catchAsync(async (req, res) => {
 });
 
 const getMe = catchAsync(async (req, res) => {
-  const token = req.headers.authorization;
+  // const token = req.headers.authorization;
 
-  if (!token) {
-    throw new AppError(httpStatus.NOT_FOUND, 'Token not found!');
-  }
+  // if (!token) {
+  //   throw new AppError(httpStatus.NOT_FOUND, 'Token not found!');
+  // }
 
-  const result = await UserServices.getMe(token);
+  const { userId, role } = req.user;
+
+  const result = await UserServices.getMe(userId, role);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -181,20 +181,20 @@ const createAdminIntoDB = async (password: string, payload: TAdmin) => {
   }
 };
 
-const getMe = async (token: string) => {
-  const decoded = verifyToken(token, config.jwt_access_secret as string);
+const getMe = async (userId: string, role: string) => {
+  // const decoded = verifyToken(token, config.jwt_access_secret as string);
 
-  const { userId, role } = decoded;
+  // const { userId, role } = decoded;
 
   let result = null;
   if (role === 'student') {
-    result = await Student.findOne({ id: userId });
+    result = await Student.findOne({ id: userId }).populate('user');
   }
   if (role === 'admin') {
-    result = await Admin.findOne({ id: userId });
+    result = await Admin.findOne({ id: userId }).populate('user');
   }
   if (role === 'faculty') {
-    result = await Faculty.findOne({ id: userId });
+    result = await Faculty.findOne({ id: userId }).populate('user');
   }
 
   // const result = await;
