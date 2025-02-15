ea7d79ba69fd2a6960e3144348f9952a100e909e
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-02-15 12:03:25 +0600

    add route for creating academic faculty with authentication and validation

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { AcademicFacultyControllers } from './academicFaculty.controller';
 import { academicFacultyValidation } from './academicFaculty.validation';
+import { USER_ROLE } from '../user/user.constant';
+import auth from '../../middlewares/auth';
 const router = express.Router();
 
 router.post(
   '/create-academic-faculty',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(
     academicFacultyValidation.createAcademicFacultyValidationSchema,
   ),
