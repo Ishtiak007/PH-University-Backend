e77fc9976e78120dd359fcbc61c2d18e2b050b28
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-02-15 19:44:04 +0600

    extend authorization to include faculty and student roles

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -57,7 +57,12 @@ router.post(
 
 router.get(
   '/me',
-  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
   UserControllers.getMe,
 );
 
