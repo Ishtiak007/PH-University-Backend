8f110f04ac2b70d73ade89414ace346352a60918
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-02-16 20:07:22 +0600

    /* eslint-disable @typescript-eslint/no-explicit-any */

--- src/app/modules/EnrolledCourse/enrolledCourse.service.ts
+++ src/app/modules/EnrolledCourse/enrolledCourse.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import mongoose from 'mongoose';
 import AppError from '../../errors/AppError';
 import { Course } from '../Course/course.model';
