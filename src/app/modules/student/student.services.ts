1084cb2aa15eb2f3f726167ddaef1c70fdae782b
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-02-16 20:32:32 +0600

    implement 'getAllStudentFromDb' function for fetching students with advanced query features

--- src/app/modules/student/student.services.ts
+++ src/app/modules/student/student.services.ts
@@ -12,12 +12,7 @@ const getAllStudentFromDb = async (query: Record<string, unknown>) => {
     Student.find()
       .populate('user')
       .populate('admissionSemester')
-      .populate({
-        path: 'academicDepartment',
-        populate: {
-          path: 'academicFaculty',
-        },
-      }),
+      .populate('academicDepartment academicFaculty'),
     query,
   )
     .search(studentSearchableFields)
@@ -26,8 +21,13 @@ const getAllStudentFromDb = async (query: Record<string, unknown>) => {
     .paginate()
     .fields();
 
+  const meta = await studentQuery.countTotal();
   const result = await studentQuery.modelQuery;
-  return result;
+
+  return {
+    meta,
+    result,
+  };
 };
 
 // get student from db
@@ -67,8 +67,6 @@ const updateStudentFromDb = async (id: string, payload: Partial<TStudent>) => {
       modifiedUpdatedData[`localGuardian.${key}`] = value;
     }
   }
-
-  console.log(modifiedUpdatedData);
 
   const result = await Student.findByIdAndUpdate(id, modifiedUpdatedData, {
     new: true,
