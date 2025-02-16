10608e28f4f119be51d19bcd3ce0534506161836
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-02-16 20:07:53 +0600

    unnecessary code removed

--- src/app/modules/EnrolledCourse/enrolledCourse.service.ts
+++ src/app/modules/EnrolledCourse/enrolledCourse.service.ts
@@ -15,13 +15,6 @@ const createEnrolledCourseIntoDB = async (
   userId: string,
   payload: TEnrolledCourse,
 ) => {
-  /**
-   * Step1: Check if the offered cousres is exists
-   * Step2: Check if the student is already enrolled
-   * Step3: Check if the max credits exceed
-   * Step4: Create an enrolled course
-   */
-
   const { offeredCourse } = payload;
 
   const isOfferedCourseExists = await OfferedCourse.findById(offeredCourse);
