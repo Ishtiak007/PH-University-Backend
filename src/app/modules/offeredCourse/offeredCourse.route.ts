56d0f8459106bae62020c0a6ca25a31b6ab67230
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-02-16 13:10:34 +0600

    add route to get all offered courses for superAdmin, admin, and faculty

--- src/app/modules/offeredCourse/offeredCourse.route.ts
+++ src/app/modules/offeredCourse/offeredCourse.route.ts
@@ -2,10 +2,16 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { OfferedCourseControllers } from './offeredCourse.controller';
 import { OfferedCourseValidations } from './offeredCourse.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
-router.get('/', OfferedCourseControllers.getAllOfferedCourses);
+router.get(
+  '/',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin, USER_ROLE.faculty),
+  OfferedCourseControllers.getAllOfferedCourses,
+);
 
 router.get('/:id', OfferedCourseControllers.getSingleOfferedCourse);
 
