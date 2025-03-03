746cb02d290ad22f28c0e3234c8a4397ced9e3b3
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-03 10:58:34 +0600

    updated academic faculty services

--- src/app/modules/academicFaculty/academicFaculty.controller.ts
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -1,6 +1,7 @@
 import httpStatus from 'http-status';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
+import { AcademicFacultyServices } from './academicFaculty.service';
 
 const createAcademicFaculty = catchAsync(async (req, res) => {
   const result = await AcademicFacultyServices.createAcademicFacultyIntoDB(
